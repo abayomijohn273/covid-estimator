14b9052e55f5b10d9d8a1e9c8fb72e83b1e7d1ba
abayomijohn273 <abayomijohn273@gmail.com>
2020-04-13 19:42:22 +0100

    add image

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import './App.css';
-
+import CovidImg from './assets/image.png';
 function App() {
   const [population, setPopulation] = useState('');
   const [timeToElapse, setTimeToElapse] = useState('');
@@ -27,91 +27,100 @@ function App() {
   return (
     <div className="main">
       <div className="container">
-        <div className="col-lg-6 col-md-6 col-sm-12">
-          <div className="number-left mt-4 mb-5">
-            <h3>Covid19 Estimator</h3>
-          </div>
-          <div className="form-frame">
-            <form onSubmit={handleSubmit}>
-              <div className="form-group mb-4">
-                <label for="data-population">Total Population</label>
-                <input
-                  type="number"
-                  name="population"
-                  value={population}
-                  onChange={e => setPopulation(e.target.value)}
-                  className="form-control"
-                  id="data-population"
-                  aria-labelledby="data-population"
-                  // placeholder="Enter Population"
-                />
-              </div>
-              <div className="form-group mb-4">
-                <label for="data-time-to-elapse">Time to Elapse</label>
-                <input
-                  type="number"
-                  name="timeToElapse"
-                  value={timeToElapse}
-                  onChange={e => setTimeToElapse(e.target.value)}
-                  className="form-control"
-                  id="data-time-to-elapse"
-                  aria-labelledby="data-time-to-elapse"
-                  // placeholder="Enter Time to elapse"
-                />
-              </div>
-              <div className="form-group mb-4">
-                <label for="data-reported-cases">Reported Cases</label>
-                <input
-                  type="number"
-                  name="reportedCases"
-                  value={reportedCases}
-                  onChange={e => setReportedCases(e.target.value)}
-                  className="form-control"
-                  id="data-reported-cases"
-                  aria-labelledby="data-reported-cases"
-                  // placeholder="Enter No of Reported Cases"
-                />
-              </div>
-              <div className="form-group mb-4">
-                <label for="data-total-hospital-beds">
-                  Total Hospital Beds
-                </label>
-                <input
-                  type="number"
-                  name="totalHospitalBeds"
-                  value={totalHospitalBeds}
-                  onChange={e => setTotalHospitalBeds(e.target.value)}
-                  className="form-control"
-                  id="data-total-hospital-beds"
-                  aria-labelledby="data-total-hospital-beds"
-                  // placeholder="Enter No of Total Hospital Bed"
-                />
-              </div>
-              <div className="form-group mb-4">
-                <label for="data-period-type">Select Period Type</label>
-                <select
-                  id="data-period-type"
-                  className="form-control"
-                  name="periodType"
-                  value={periodType}
-                  onChange={e => setPeriodType(e.target.value)}
-                  aria-labelledby="data-period-type"
+        <div className="row align-items-center">
+          <div className="col-lg-5 col-md-5 col-sm-12">
+            <div className="number-left mt-4 mb-5">
+              <h3>Covid19 Estimator</h3>
+            </div>
+            <div className="form-frame">
+              <form onSubmit={handleSubmit}>
+                <div className="form-group mb-4">
+                  <label for="data-population">Total Population</label>
+                  <input
+                    type="number"
+                    name="population"
+                    value={population}
+                    onChange={e => setPopulation(e.target.value)}
+                    className="form-control"
+                    id="data-population"
+                    aria-labelledby="data-population"
+                    // placeholder="Enter Population"
+                  />
+                </div>
+                <div className="form-group mb-4">
+                  <label for="data-time-to-elapse">Time to Elapse</label>
+                  <input
+                    type="number"
+                    name="timeToElapse"
+                    value={timeToElapse}
+                    onChange={e => setTimeToElapse(e.target.value)}
+                    className="form-control"
+                    id="data-time-to-elapse"
+                    aria-labelledby="data-time-to-elapse"
+                    // placeholder="Enter Time to elapse"
+                  />
+                </div>
+                <div className="form-group mb-4">
+                  <label for="data-reported-cases">Reported Cases</label>
+                  <input
+                    type="number"
+                    name="reportedCases"
+                    value={reportedCases}
+                    onChange={e => setReportedCases(e.target.value)}
+                    className="form-control"
+                    id="data-reported-cases"
+                    aria-labelledby="data-reported-cases"
+                    // placeholder="Enter No of Reported Cases"
+                  />
+                </div>
+                <div className="form-group mb-4">
+                  <label for="data-total-hospital-beds">
+                    Total Hospital Beds
+                  </label>
+                  <input
+                    type="number"
+                    name="totalHospitalBeds"
+                    value={totalHospitalBeds}
+                    onChange={e => setTotalHospitalBeds(e.target.value)}
+                    className="form-control"
+                    id="data-total-hospital-beds"
+                    aria-labelledby="data-total-hospital-beds"
+                    // placeholder="Enter No of Total Hospital Bed"
+                  />
+                </div>
+                <div className="form-group mb-4">
+                  <label for="data-period-type">Select Period Type</label>
+                  <select
+                    id="data-period-type"
+                    className="form-control"
+                    name="periodType"
+                    value={periodType}
+                    onChange={e => setPeriodType(e.target.value)}
+                    aria-labelledby="data-period-type"
+                  >
+                    <option>Choose...</option>
+                    <option value="days">Days</option>
+                    <option value="weeks">Weeks</option>
+                    <option value="months">Months</option>
+                  </select>
+                </div>
+                <button
+                  type="submit"
+                  className="btn btn-lg mb-4"
+                  id="data-go-estimate"
+                  aria-labelledby="data-go-estimate"
                 >
-                  <option>Choose...</option>
-                  <option value="days">Days</option>
-                  <option value="weeks">Weeks</option>
-                  <option value="months">Months</option>
-                </select>
-              </div>
-              <button
-                type="submit"
-                className="btn btn-lg mb-4"
-                id="data-go-estimate"
-                aria-labelledby="data-go-estimate"
-              >
-                Submit Data
-              </button>
-            </form>
+                  Submit Data
+                </button>
+              </form>
+            </div>
+          </div>
+          <div className="col-lg-6 col-md-6 hidden-sm hidden-xs center m-auto">
+            <img
+              class="img-responsive covid-img"
+              src={CovidImg}
+              alt="Covid19 "
+            />
           </div>
         </div>
       </div>
